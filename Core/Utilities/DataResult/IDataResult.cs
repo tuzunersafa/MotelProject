e643626faf434bd7e71636b672ddaa79a03ef8f7
tuzunersafa <safatuzuner@gmail.com>
2023-08-12 15:41:02 +0300

    Result sistemi eklendi.

---
+++ Core/Utilities/DataResult/IDataResult.cs
@@ -0,0 +1,14 @@
+﻿using Core.Utilities.VoidResult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.DataResult
+{
+    public interface IDataResult<T> : IResult
+    {
+        T Data { get; }
+    }
+}
---
+++ Core/Utilities/VoidResult/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.VoidResult
+{
+    public class Result : IResult
+    {
+        public Result(string message, bool success) : this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            IsSuccess = success;
+        }
+
+        public string Message { get; }
+
+        public bool IsSuccess { get; }
+    }
+}
